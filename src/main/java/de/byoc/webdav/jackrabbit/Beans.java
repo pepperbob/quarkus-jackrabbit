e171880834f802689eb1d0142a2bdf2809bba92d
pepperbob <github@byoc.de>
2021-06-16 21:05:55 +0200

    chore: cleanups

--- src/main/java/de/byoc/webdav/jackrabbit/Beans.java
+++ src/main/java/de/byoc/webdav/jackrabbit/Beans.java
@@ -6,13 +6,20 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
 import javax.jcr.Repository;
 import javax.jcr.RepositoryException;
+import java.util.Map;
+
+import static org.apache.jackrabbit.core.RepositoryFactoryImpl.REPOSITORY_CONF;
+import static org.apache.jackrabbit.core.RepositoryFactoryImpl.REPOSITORY_HOME;
 
 public class Beans {
 
   @ApplicationScoped
   @Produces
   public Repository repo() throws RepositoryException {
-    return JcrUtils.getRepository();
+    return JcrUtils.getRepository(Map.of(
+            REPOSITORY_HOME, "schnuffel-repo",
+            REPOSITORY_CONF, "classpath:my_repository.xml"
+    ));
   }
 
 
